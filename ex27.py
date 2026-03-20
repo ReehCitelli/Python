518fbd146d09037e9d965b2b81bf6a81b1c38040
Renata Citelli <47578656+ReehCitelli@users.noreply.github.com>
2026-03-20 16:22:06 -0300

    Primeiro e último nome de uma pessoa
    Exercício Python 27: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.

---
+++ ex27.py
@@ -0,0 +1,5 @@
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print('Muito prezer em tem conhecer!')
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu ultimo nome é {}'.format(nome[len(nome)-1]))
