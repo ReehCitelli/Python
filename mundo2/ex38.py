a26891f7844648f4713fa43d3fbe6e78bb77d469
Renata Citelli <47578656+ReehCitelli@users.noreply.github.com>
2026-07-28 16:22:20 -0300

    Comparando números
    Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
    
    – O primeiro valor é maior
    
    – O segundo valor é maior

---
+++ mundo2/ex38.py
@@ -0,0 +1,9 @@
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+
+if n1 > n2:
+    print('O primeiro número é maior')
+elif n1 < n2:
+    print('O segundo número é maior')
+else:
+    print('Os dois números são iguais')
