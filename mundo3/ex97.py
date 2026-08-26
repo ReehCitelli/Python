c2f9e78ce7820ba168e4bf394fe20324ea80d2e9
Renata Citelli <reehcitelli@gmail.com>
2026-08-26 15:43:33 -0300

    feat: Um print especial

---
+++ mundo3/ex97.py
@@ -0,0 +1,11 @@
+def escreva (msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+escreva('Olá, Mundo!')
+escreva('Curso de Python no YouTube')
+escreva('Renata')
+escreva('Aprenda Python')
