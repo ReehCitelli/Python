101bb9f1ba0c70151421e787de83855c36e57b2c
Renata Citelli <reehcitelli@gmail.com>
2026-08-28 15:36:40 -0300

    feat: Funções para votação

---
+++ mundo3/ex101.py
@@ -0,0 +1,14 @@
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NAO VOTA!'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL!'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATORIO'
+
+
+nasc = int(input('Em que ano voce nasceu? '))
+print(voto(nasc))
