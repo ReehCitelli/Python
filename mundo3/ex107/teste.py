1a0a1f26b8ecaf74056c0832bdc211b14769c8ac
Renata Citelli <reehcitelli@gmail.com>
2026-09-02 15:39:12 -0300

    feat: aula de modulos

---
+++ mundo3/ex107/teste.py
@@ -0,0 +1,6 @@
+import moeda
+
+p = float(input('Digite o preço: R$'))
+print(f'A metade de R${p} é R${moeda.metade(p)}')
+print(f'O dobro de R${p} é R${moeda.dobro(p)}')
+print(f'Almentando 10%,temos R${moeda.aumentar(p, 10)}')
